41f691c2ab0a0a529f5d01173437e2241af294fa
dallasjames <dallasjames42@gmail.com>
2023-03-15 14:44:29 -0600

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZZS4BXLLNW"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZZS4BXLLNW', {
+          page_path: window.location.pathname,
+          });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
